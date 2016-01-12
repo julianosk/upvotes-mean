671ccb269697840bc44ad9059a356a4b40de8194
Juliano Krieger <julianosk@gmail.com>
2016-01-12 17:21:42 +0000

    starting posts

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 // var angular = require('angular')
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: '/home.html',
+				controller: 'MainCtrl'
+			});
+			.state('posts', {
+				url: '/posts/{id}',
+				templateUrl: 'posts.html',
+				controller: 'PostsCtrl'
+			});
+
+		$urlRouterProvider.otherwise('home');
+	}
+]);
 
 app.factory('posts',[function(){
 	var o = {
