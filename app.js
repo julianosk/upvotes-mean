803b747835fb38112f65ad4f97d70f711c5897a6
Juliano Krieger <julianosk@gmail.com>
2016-01-19 17:35:46 +0000

    Posts controller

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.config([
 				url: '/home',
 				templateUrl: '/home.html',
 				controller: 'MainCtrl'
-			});
+			})
 			.state('posts', {
 				url: '/posts/{id}',
 				templateUrl: 'posts.html',
@@ -46,7 +46,11 @@ app.controller('MainCtrl', [
 			$scope.posts.push({
 				title: $scope.title, 
 				link: $scope.link,
-				upvotes: 0
+				upvotes: 0,
+				comments: [
+					{author: 'Joe', body: 'Cool post!', upvotes:0},
+					{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes:0}
+				]
 			});
 			$scope.title = '';
 			$scope.link = '';
@@ -54,5 +58,14 @@ app.controller('MainCtrl', [
 		$scope.incrementUpvotes = function(post) {
 			post.upvotes += 1;
 		};
+	}
+]);
+
+app.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts) {
+		$scope.post = posts.posts[$stateParams.id]
 	}
 ]);
