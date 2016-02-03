65b4f6c3ff0cc5d25526a6217d81c617d5f578b2
Juliano Krieger <julianosk@gmail.com>
2016-02-03 17:29:05 +0000

    Finishing Off Comments

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -19,7 +19,12 @@ app.config([
 			.state('posts', {
 				url: '/posts/{id}',
 				templateUrl: 'posts.html',
-				controller: 'PostsCtrl'
+				controller: 'PostsCtrl',
+				resolve: {
+					post: ['$stateParams', 'posts', function($stateParams, posts) {
+						return posts.get($stateParams.id);
+					}]
+				}
 			});
 
 		$urlRouterProvider.otherwise('home');
@@ -35,6 +40,31 @@ app.factory('posts',['$http', function($http){
 			angular.copy(data, o.posts);
 		});
 	};
+	o.create = function(post) {
+		return $http.post('/posts', post).success(function(data){
+			o.posts.push(data);
+		});
+	};
+	o.upvote = function(post) {
+		return $http.put('/posts/' + post._id + '/upvote')
+			.success(function(data) {
+				post.upvotes += 1;
+			});
+	};
+	o.get = function(id) {
+		return $http.get('/posts/' + id).then(function(res) {
+			return res.data;
+		});
+	};
+	o.addComment = function(id, comment) {
+		return $http.post('/posts/' + id + '/comments', comment);
+	};
+	o.upvoteComment = function(post, comment) {
+		return $http.put('/posts/' + post._id + '/comments/' + comment._id +'/upvote')
+			.success(function(data) {
+				comment.upvotes += 1;
+			});
+	};
 	return o;
 }]);
 
@@ -45,35 +75,37 @@ app.controller('MainCtrl', [
 		$scope.posts = posts.posts;
 		$scope.addPost = function() {
 			if (!$scope.title || $scope.title === '') { return; }
-			$scope.posts.push({
+			posts.create({
 				title: $scope.title,
-				link: $scope.link,
-				upvotes: 0,
-				comments: []
+				link: $scope.link
 			});
 			$scope.title = '';
 			$scope.link = '';
 		};
 		$scope.incrementUpvotes = function(post) {
-			post.upvotes += 1;
+			posts.upvote(post);
 		};
 	}
 ]);
 
 app.controller('PostsCtrl', [
 	'$scope',
-	'$stateParams',
 	'posts',
-	function($scope, $stateParams, posts) {
-		$scope.post = posts.posts[$stateParams.id];
+	'post',
+	function($scope, posts, post) {
+		$scope.post = post;
 		$scope.addComment = function(){
-			if ($scope.body === '') {return;}
-			$scope.post.comments.push({
+			if ($scope.body === '') { return; }
+			posts.addComment(post._id, {
 				body: $scope.body,
 				author: 'user',
-				upvotes: 0
+			}).success(function(comment) {
+				$scope.post.comments.push(comment);
 			});
 			$scope.body = '';
+		};
+		$scope.incrementUpvotes = function(comment) {
+			posts.upvoteComment(post, comment);
 		};
 	}
 ]);
